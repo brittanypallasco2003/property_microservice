e3a540dd84c42b9f4a74eca4825c9a5e54154e37
brittanypallasco2003 <nohemiespinel0@gmail.com>
2025-01-16 16:46:26 -0500

    feat: create property microservice

---
+++ src/main/java/com/spring_boot_microservice_1_property/SpringBootMicroservice1PropertyApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot_microservice_1_property;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice1PropertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice1PropertyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_microservice_1_property/SpringBootMicroservice1PropertyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_microservice_1_property;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMicroservice1PropertyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
